7c02e009a7277550033b0a0a123e5e23ecff2857
So00s <alex.volkovv2yandex.ru>
2023-05-20 20:12:14 +0300

    20.12

--- parsers_url.py
+++ parsers_url.py
@@ -35,29 +35,48 @@ def parse_of_daylight_hours(region):
 ай': 'Красноярск', 'Пермский край': 'Пермь', 'Приморский край': 'Владивосток', 'Ставропольский край': 'Ставропол\
 ь', 'Хабаровский край': 'Хабаровск', 'Ненецкий автономный округ': 'Нарьян-Мар', 'Ханты-Мансийский автономный окру\
 г': 'Ханты-Мансийск', 'Чукотский автономный округ': 'Анадырь', 'Ямало-Ненецкий автономный округ': 'Салехард', 'Еврейска\
-я автономная область':	'Биробиджан'}
+я автономная область': 'Биробиджан'}
     gc = geonamescache.GeonamesCache()
     city_name = dict_of_cites.get(region)
     print(city_name)
     countries = gc.search_cities(city_name, case_sensitive=True)
     id_of_city = countries[0]['geonameid']
+    sum_of_day = 0
+    avg_summer = datetime.timedelta()
+    avg_autumn = datetime.timedelta()
+    avg_winter = datetime.timedelta()
+    avg_spring = datetime.timedelta()
     for month in range(1, 13):
         url = f'https://dateandtime.info/ru/citysunrisesunset.php?id={id_of_city}&month={month}&year=2023'
         page = requests.get(url)
         soup = BeautifulSoup(page.text, "html.parser")
         table = soup.findAll('tbody')[1]
         all_rows = table.findAll('tr')
-        print(month)
         mysum = datetime.timedelta()
         for _ in all_rows:
             day_length = _.findAll('td')[4].text
             day_length = day_length[0:5]
-            print(day_length)
             h, m = day_length.split(':')
             d = datetime.timedelta(hours=int(h), minutes=int(m))
+            if 3 <= month <= 5:
+                avg_spring += d
+            elif 6 <= month <= 8:
+                avg_summer += d
+            elif 9 <= month <= 11:
+                avg_autumn += d
+            else:
+                avg_winter += d
             mysum += d
             days = mysum.days
         print(f"месяц {month} дней {days} ")
+        sum_of_day += days
+    avg_autumn = round(avg_autumn / datetime.timedelta(days=91) * 24)
+    avg_summer = round(avg_summer / datetime.timedelta(days=92) * 24)
+    avg_spring = round(avg_spring / datetime.timedelta(days=92) * 24)
+    avg_winter = round(avg_winter / datetime.timedelta(days=90) * 24)
+    info_of_days = [avg_summer, avg_winter, avg_spring, avg_autumn, sum_of_day]
+    return info_of_days
+
 
 def parse_of_red_book(name):
     url = f'https://www.plantarium.ru/page/search.html'
@@ -83,5 +102,4 @@ def parse_of_red_book(name):
         for _ in all_redbooks[1:]:
             if _.text[:-7] not in list_of_red_books:
                 list_of_red_books.append(_.text[:-7])
-    print(list_of_red_books)
-
+    return list_of_red_books
